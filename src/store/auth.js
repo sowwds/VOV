ec93b100e91be811ec9abae94d519181762f396b
sowwds <aleksey4094@yandex.ru>
2025-05-12 18:59:00 +0300

    made profile info

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,20 +1,27 @@
 import { defineStore } from 'pinia';
 import api from '@/services/api';
+import avatarPlaceholder from '@/assets/avatar-placeholder.png'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     token: localStorage.getItem('token') || null,
     user: null,
   }),
+  getters: {
+    avatarUrl: (state) => state.user?.avatar_url || avatarPlaceholder,
+    fullName: (state) => state.user?.name || 'Неизвестный пользователь',
+    email: (state) => state.user?.email || 'user@example.com',
+  },
   actions: {
     async fetchUser() {
       try {
-        const response = await api.get('/auth/profile'); // Укажите правильный эндпоинт
-        console.log('пользователь получен')
-        this.user = response.data.user;
+        const response = await api.get('/auth/profile');
+        console.log(response);
+        this.user = response.data;
       } catch (error) {
         console.error('Ошибка при получении данных пользователя:', error);
         this.user = null;
+        this.logout();
       }
     },
     setToken(token) {
