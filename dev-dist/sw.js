772d37404698b6333f0f465042cc552b12e7c907
sowwds <aleksey4094@yandex.ru>
2025-05-26 12:07:27 +0300

    quick fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5f6b949'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m15dg63911g"
+    "revision": "0.e8glnqv4h0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
