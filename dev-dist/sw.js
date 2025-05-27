0d457a8eaff1c2f60819056a8a8a779b95bbcb0c
sowwds <aleksey4094@yandex.ru>
2025-05-27 22:30:16 +0300

    homepage animation fix on mobile

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5f6b949'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g434nndi1j"
+    "revision": "0.ahrarcn8o3"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
